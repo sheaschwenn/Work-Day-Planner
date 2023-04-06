c54d711c4ff8aae9db51ec5a74258891719b798d
Shea Schwenn <sheaschwenn@gmail.com>
2023-04-05 19:12:13 -0700

    user input now saved, commented out code

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,5 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// the code isn't run until the browser has finished rendering all the elements in the html.
 $(function () {
   var past = $(".past");
   var present = $(".present");
@@ -13,44 +12,36 @@ $(function () {
   var textEl = $(".description");
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
   console.log(hour)
   console.log(timeBlock.attr("id"))
-  saveButton.on("click",function (event) {
+
+  saveButton.on("click", function (event) {
+    // pointing to the button that was clicked, and then points the value in it sibling with class descrition
     var text = $(this).siblings(".description").val()
+    // the id of the timeblock that the button that was clicked
     var timeId = $(this).parent().attr("id")
-    // console.log(button.prev().val())
-  //  console.log(unique)
-  //  console.log(button)
-    // need to point to the text area 
-    // how can use this to point to the unique id for each time block
-    // need this to be the key so that when i getitem i can get item for that same timeblock
-      //  localStorage.setItem("toDo", $(this).attr("id").text())
-      // create an object that creates a new key for each id and then the associated string get paired with it 
-      localStorage.setItem(timeId,text )
-      // localStorage.setItem(timeId,JSON.stringify(text) )
+
+    // don't have to stringify since not an object and input is a string 
+    // setting the key to be the id of the textblock that the user typed in
+    // setting the value to be the users input 
+    localStorage.setItem(timeId, text)
+    // this also works
+    // localStorage.setItem(timeId,JSON.stringify(text) )
   })
 
 
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // function for setting the class of each hour 
   function hourClass() {
     console.log("is this function running ")
-
+    // iterating through each element with class of time-block
     timeBlock.each(function () {
       // need to compare number to number not to string 
       // substring cuts the string at index 5 and leaves us with just the number of the hour
       var timeHour = parseInt($(this).attr("id").substring(5))
-      console.log(timeHour , hour)
+      // console.log(timeHour, hour) check if hour is the same as the parsed substring of the id
+
+      // comparing the current hour to each timeblock id 
       if (timeHour === hour) {
         console.log(timeHour)
         $(this).addClass("present")
@@ -71,23 +62,27 @@ $(function () {
 
 
 
-
+  // calling the function
   hourClass()
 
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  // timeBlock.each(function(){
-  //   localStorage.setItem($(this.attr("id").text))
-
-  // })
+  // dont need to parse since never stringifyed 
+  // setting the value of each description to the users input
+  // saved even through refresh 
   // $("#hour-9 .description").val(JSON.parse(localStorage.getItem("hour-9")))
   $("#hour-9 .description").val(localStorage.getItem("hour-9"))
-//  .text($(this))
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"))
 
 
-  // TODO: Add code to display the current date in the header of the page.
+
+
+  // setting the text of the id currentday as the current day 
   $("#currentDay").text(today)
 });
